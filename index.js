f2fc7e80b2fd5336a889df06df686ce42114f9ae
lostsoup <oliverowen2210@gmail.com>
2022-01-11 17:08:13 -0600

    reworked code, added basic ai

--- index.js
+++ index.js
@@ -1,53 +1,130 @@
-CSSBoard = document.querySelector('#board');
 CSSGameObjects = document.querySelectorAll('.gameobject');
-reset = document.querySelector('#reset');
 
-const player = (mark) => ({
-  mark,
+const player = (name, marker, CPU=false) => {
+let playing = true;
 
-  markObject(object) {
-    object.classList.add(`${mark}marker`)
-  },
-})
-
-const you = (() => {
-  return Object.assign({}, player('x'))
-})();
-
-const computer = (() => {
-  return Object.assign({}, player('o'))
-})();
-
-const gameBoard = (function() {
-  gameObjects = [];
-  turn = 'x';
-  return {gameObjects, turn};
-
-})();
-
-const gameObject = (id) => {
-  let data = {
-    id,
-    x: (Math.floor((id-1)%3)),
-    y: (Math.floor((id-1)/3)),
-    marker: 2,
+let data = {
+    name,
+    marker, //'x' or 'o'
+    CPU,
   };
 
-  getPos = () => {
-    return [data.x, data.y];
-  }
-  return {getPos};
-}
+  const getMarker = () => {
+    return data.marker;
+  };
 
+  const markSquare = (target) => {
+    target.mark(data.marker);
+    game.endTurn();
+  };
+  const changeTurn = (state=undefined) => {
+    if (state||state===false) playing = state;
+    else playing = !playing;
+  }
+
+  const CPURound = () => {
+    let validSquares = [];
+    if (data.CPU) {
+      for (let square of game.squares) {
+        if (!square.getMark()) {
+          validSquares.push(square.getId());
+          console.log(validSquares)
+      }};
+      choice = validSquares[Math.floor(Math.random() * validSquares.length)]
+      console.log(choice);
+      for (let square of game.squares) {
+        if (square.getId() == choice) {
+          setTimeout(function () {markSquare(square)}, 1000);
+          return;
+        };
+    }};
+  };
+  return {playing, markSquare, getMarker, changeTurn, CPURound};
+};
+
+const square = (selector) => {
+  const html = document.querySelector(selector);
+
+  let marker = '';
+  let id = html.dataset.id;
+
+  const clearMarkers = () => {
+    html.classList.remove('xmarker');
+    html.classList.remove('omarker');
+    html.classList.remove('hover');
+  };
+
+  const mark = (m) => {
+    if (!marker){
+      clearMarkers()
+      marker = m;
+      html.classList.add(`${marker}marker`);
+    }
+  };
+
+  const getMark = () => {
+    return marker;
+  }
+
+  const getId = () => {
+    return Number(id);
+  }
+
+  return {html, mark, getMark, getId};
+};
+
+const game = (() => {
+  let squares = [];
+  let turn = 'x';
+
+  const getTurn = () => turn;
+
+  const endTurn = () => {
+    turn = (turn=='x') ? 'o' : 'x';
+    if (turn == 'o') computer.CPURound();
+  }
+
+  const initPlayers = () => {
+    you = Object.assign({}, player('You', 'x'));
+    computer = Object.assign({}, player('CPU', 'o', true));
+  }
+
+  const initSquares = () => {
+    for (let square of squares) {
+
+      square.html.addEventListener('mouseover', (e) => {
+
+      if (!square.html.classList.contains('xmarker') &&
+      !square.html.classList.contains('omarker') &&
+      game.getTurn() == 'x') {
+
+        square.html.classList.add('hover');
+
+      }});
+
+      square.html.addEventListener('mouseout', (e) => {
+        square.html.classList.remove('hover');
+
+      });
+
+      square.html.addEventListener('click', (e) => {
+        if (game.getTurn() == 'x') {
+         you.markSquare(square);
+      }});
+    }};
+
+    const init = () => {
+      initSquares();
+      initPlayers();
+    }
+    
+
+  return {getTurn, endTurn, squares, init};
+})();
 
 CSSGameObjects.forEach((obj) => {
-  gameBoard.gameObjects.push(gameObject(Number(obj.dataset.id)));
-  obj.addEventListener('mouseover', (e) => {
-    if (!obj.classList.contains('xmarker') && !obj.classList.contains('omarker')) {
-    obj.classList.add('hover')
-    };
-  });
-  obj.addEventListener('mouseout', (e) => obj.classList.remove('hover'))
-  obj.addEventListener('click', (e) => you.markObject(obj));
+  game.squares.push(square(`[data-id='${obj.dataset.id}']`));
 });
 
+
+game.init();
