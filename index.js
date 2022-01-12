ebdb470a7fbda3541a1d86ed91459da9eb4e0bd8
lostsoup <oliverowen2210@gmail.com>
2022-01-12 08:11:56 -0600

    tidied up code, row column and diagonal checks, square painting, and non-functional tallies

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 CSSGameObjects = document.querySelectorAll('.gameobject');
 
-const player = (name, marker, CPU=false) => {
-let playing = true;
+const player = (name, marker, color, score, CPU=false) => {
 
 let data = {
     name,
     marker, //'x' or 'o'
+    color,
+    score,
     CPU,
-  };
-
-  const getMarker = () => {
-    return data.marker;
   };
 
   const markSquare = (target) => {
     target.mark(data.marker);
-    game.endTurn();
+    game.endTurn(data.name);
   };
-  const changeTurn = (state=undefined) => {
-    if (state||state===false) playing = state;
-    else playing = !playing;
-  }
 
   const CPURound = () => {
     let validSquares = [];
@@ -39,11 +32,21 @@ let data = {
         };
     }};
   };
-  return {playing, markSquare, getMarker, changeTurn, CPURound};
+
+  const getName = () => data.name;
+
+  const getColor = () => data.color;
+
+  const getMarker = () =>  data.marker;
+
+  return {markSquare, CPURound, getName, getColor, getMarker};
 };
 
 const square = (selector) => {
   const html = document.querySelector(selector);
+
+  const xPos = Math.floor(Number((html.dataset.id-1)%3));
+  const yPos = Math.floor(Number((html.dataset.id-1)/3));
 
   let marker = '';
   let id = html.dataset.id;
@@ -62,32 +65,129 @@ const square = (selector) => {
     }
   };
 
-  const getMark = () => {
-    return marker;
+  const getMark = () => marker;
+
+  const getId = () => Number(id);
+
+  const getPosition = () => [xPos, yPos];
+
+  const setBG = (color) => {
+    html.style.backgroundColor = color;
   }
 
-  const getId = () => {
-    return Number(id);
-  }
-
-  return {html, mark, getMark, getId};
+  return {html, mark, getMark, getId, getPosition, setBG};
 };
 
 const game = (() => {
   let squares = [];
-  let turn = 'x';
+  let players = [];
+  let turn = 0;
 
   const getTurn = () => turn;
 
   const endTurn = () => {
-    turn = (turn=='x') ? 'o' : 'x';
-    if (turn == 'o') computer.CPURound();
+    if (r = checkSquares()) {
+      setTimeout(function() {win(players[turn].getName(), r)}, 500);
+    }
+    else {
+      switch(turn) {
+        case null:
+          break;
+        case 0:
+          turn = 1;
+          computer.CPURound();
+          break;
+        case 1:
+          turn = 0;
+          break;
+      };
+    };
+  };
+
+  const paintSquares = (data) => {
+    color = players[turn].getColor()
+    switch(data[1]) {
+    case 'column':
+      for(let i = data[0];i<9;i+=3) {
+        game.squares[i].setBG(color);
+      };
+      break;
+
+    case 'row':
+      let j = data[0]+3;
+      for(let i = data[0];i<j;i++) {
+        game.squares[i].setBG(color);
+      };
+      break;
+    
+    case 'back':
+      for(let i=0;i<=8;i+=4) {
+        game.squares[i].setBG(color);
+      };
+      break;
+
+    case 'forward':
+      for(let i=2;i<=6;i+=2) {
+        game.squares[i].setBG(color);
+      };
+      break;
+    };
+  };
+
+  const win = (name, paintData) => {
+    paintSquares(paintData);
+    endGame();
   }
 
+  const checkColumns = () => {
+    for(let i=0;i<3;i++) {
+      let m = game.squares[i].getMark();
+      if (!m) continue;
+      if (game.squares[i+3].getMark() == m && 
+          game.squares[i+6].getMark() == m) return [i, 'column'];
+    };
+  };
+
+  const checkRows = () => {
+    for(let i=0;i<7;i+=3) {
+      let m = game.squares[i].getMark();
+      if (!m) continue;
+      if (game.squares[i+1].getMark() == m &&
+          game.squares[i+2].getMark() == m) return [i, 'row'];
+    };
+  };
+
+  const checkDiagonals = () => {
+      let m = game.squares[0].getMark();
+      if (m) {
+        if (game.squares[4].getMark() == m &&
+          game.squares[8].getMark() == m) return [0, 'back'];
+      }
+
+      m = game.squares[2].getMark();
+      if (m) {
+        if (game.squares[4].getMark() == m &&
+            game.squares[6].getMark() == m) return [0, 'forward'];
+      }
+  };
+  
+
+  const checkSquares = () => {
+    winCheck = checkColumns();
+    if (!winCheck) winCheck = checkRows();
+    if (!winCheck) winCheck = checkDiagonals();
+    return winCheck;
+  };
+
+  const endGame = () => {
+    turn = null;
+  };
+
   const initPlayers = () => {
-    you = Object.assign({}, player('You', 'x'));
-    computer = Object.assign({}, player('CPU', 'o', true));
-  }
+    you = Object.assign({}, player('you', 'x', 'green', 0));
+    computer = Object.assign({}, player('computer', 'o', 'red', 0, true));
+    players.push(you, computer);
+  };
 
   const initSquares = () => {
     for (let square of squares) {
@@ -96,7 +196,7 @@ const game = (() => {
 
       if (!square.html.classList.contains('xmarker') &&
       !square.html.classList.contains('omarker') &&
-      game.getTurn() == 'x') {
+      game.getTurn() == 0) {
 
         square.html.classList.add('hover');
 
@@ -108,7 +208,8 @@ const game = (() => {
       });
 
       square.html.addEventListener('click', (e) => {
-        if (game.getTurn() == 'x') {
+        if (game.getTurn() == 0 &&
+            square.getMark() == '') {
          you.markSquare(square);
       }});
     }};
@@ -116,7 +217,7 @@ const game = (() => {
     const init = () => {
       initSquares();
       initPlayers();
-    }
+    };
     
 
   return {getTurn, endTurn, squares, init};
